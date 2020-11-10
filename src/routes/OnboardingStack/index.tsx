48c23b63eb1bc68db33a15c81fd0939ce562d151
AugustoMarcelo <mrclgst10@gmail.com>
2020-11-10 20:54:15 -0300

    :lipstick: updated screens order

--- src/routes/OnboardingStack/index.tsx
+++ src/routes/OnboardingStack/index.tsx
@@ -4,6 +4,9 @@ import { createStackNavigator } from '@react-navigation/stack';
 
 import Onboarding from '../../pages/Onboarding';
 import Welcome from '../../pages/Welcome';
+import Login from '../../pages/Login';
+import Register from '../../pages/Register';
+import RegisterSuccessful from '../../pages/RegisterSuccessful';
 
 const { Navigator, Screen } = createStackNavigator();
 
@@ -13,6 +16,9 @@ export default function OnboardingStack() {
       <Navigator screenOptions={{ headerShown: false }}>
         <Screen name="Onboarding" component={Onboarding} />
         <Screen name="Welcome" component={Welcome} />
+        <Screen name="Login" component={Login} />
+        <Screen name="Register" component={Register} />
+        <Screen name="RegisterSuccessful" component={RegisterSuccessful} />
       </Navigator>
     </NavigationContainer>
   );
