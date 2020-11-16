117104f62a4792d500a2f78c0be080efaf23e443
AugustoMarcelo <mrclgst10@gmail.com>
2020-11-16 16:55:43 -0300

    :bento: Added app icons

---
+++ src/images/icons/index.ts
@@ -0,0 +1,15 @@
+// Icons to Bottom Menu
+export { default as CarIcon } from './car.svg';
+export { default as HomeIcon } from './home.svg';
+export { default as CalendarIcon } from './calendar.svg';
+export { default as UserIcon } from './user.svg';
+
+export { default as DropletIcon } from './droplet.svg';
+export { default as EnergyIcon } from './energy.svg';
+export { default as LeafIcon } from './leaf.svg';
+
+export { default as LockIcon } from './lock.svg';
+export { default as MagnifyingGlassIcon } from './magnifying_glass.svg';
+export { default as MailIcon } from './mail.svg';
+export { default as SlidersIcon } from './sliders.svg';
+export { default as CameraIcon } from './camera.svg';
